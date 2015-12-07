d6d70ba75178ce359cf64dc0c958050cef8bfc07
darksamus6 <darksamus6@gmail.com>
2015-12-07 23:47:40 +0000

    Initial set up of mailer

---
+++ test/mailers/notication_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class NoticationMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/mailers/previews/notication_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/notication_mailer
+class NoticationMailerPreview < ActionMailer::Preview
+
+end
