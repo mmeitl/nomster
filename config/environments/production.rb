055be6fa2acf29d660e98200b4a2d908dedecc0f
darksamus6 <darksamus6@gmail.com>
2015-12-08 02:07:17 +0000

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => 'nomster-matt-meitl.heroku.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
