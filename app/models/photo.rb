861c5aa314de3e23474a75b4b327f95a017bba17
darksamus6 <darksamus6@gmail.com>
2015-12-08 19:48:05 +0000

    thumbnails

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,7 @@ class Photo < ActiveRecord::Base
     belongs_to :user
     belongs_to :place
     
+    validates :caption,  length: { minimum: 3 }
+    validates :picture, :presence => true
     mount_uploader :picture, PictureUploader
 end
